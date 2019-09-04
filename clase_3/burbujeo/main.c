79932ca2d406c2d486b9686cc1d03a1bea8702ea
alumno <lucianogreco92@gmail.com>
2019-09-03 22:06:06 -0300

    clase 3

---
+++ clase_3/burbujeo/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vec[5] = {7,5,6,3,2};
+    int aux = 0 , i , j;
+
+    for(i=0;i<5-1;i++)
+    {
+        for(j=i+1;j<5;j++)
+        {
+            if(vec[i]>vec[j])
+            {
+                aux=vec[i];
+                vec[i]=vec[j];
+                vec[j]=aux;
+            }
+        }
+
+    }
+
+
+
+    return 0;
+}
